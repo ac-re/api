bac6d240cc4cbf4f037cc2aafeb5fe4d06c85c8d
developer <june@JunedeMacBook-Pro.local>
2024-10-17 13:14:28 +0800

    split emailFrom from emailServer

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,102 +1,103 @@
-'use strict'
-const nodemailer = require('nodemailer')
-const config = require('../helpers/config')
-const contactFormTemplate = require('../helpers/contactFormTemplate')
-const careerFormTemplate = require('../helpers/careerFormTemplate')
+"use strict";
+const nodemailer = require("nodemailer");
+const config = require("../helpers/config");
+const contactFormTemplate = require("../helpers/contactFormTemplate");
+const careerFormTemplate = require("../helpers/careerFormTemplate");
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: config.emailserver,
-        pass: config.password
-    },
-})
+  service: "gmail",
+  auth: {
+    user: config.emailserver,
+    pass: config.password,
+  },
+});
 
 const debugView = (req, res, next) => {
-    res.render('layout', { 
-        apiUrl: config.apiUrl,
-        emailserver: config.emailserver,
-        emailto: config.emailto,
-        password: config.password,
-        contactemailto: config.contactEmailto
-    })
-}
+  res.render("layout", {
+    apiUrl: config.apiUrl,
+    emailserver: config.emailserver,
+    emailFrom: config.emailFrom,
+    emailto: config.emailto,
+    password: config.password,
+    contactemailto: config.contactEmailto,
+  });
+};
 
 const contactForm = async (req, res) => {
-    if (req.method === 'POST') {
-        const data = req.body;
-        if (!data.name || !data.email || !data.message) {
-          return res.status(422).json({ message: 'content missed' });
-        }
+  if (req.method === "POST") {
+    const data = req.body;
+    if (!data.name || !data.email || !data.message) {
+      return res.status(422).json({ message: "content missed" });
+    }
 
-        if (data.name === '400: test form fail') {
-            return res.status(400).json({ message: 'Bad request' })
-        }
-    
-        try {
-          const contactMailOptions = {
-            from: config.emailserver,
-            to: config.contactEmailto,
-            cc: data.email,
-          }  
-          const mailData = {
-            ...contactMailOptions,
-            subject: `Contact form: ${data.subject} by-${data.company} ${data.name}`,
-            html: contactFormTemplate(data),
-          };
-          await transporter.sendMail(mailData);
-    
-          return res.status(200).json({ success: true });
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.log(error);
-          return res.status(400).json({ message: 'Bad request' });
-        }
-      }
-    
-      return res.status(400).json({ message: 'Bad request' });
-}
+    if (data.name === "400: test form fail") {
+      return res.status(400).json({ message: "Bad request" });
+    }
+
+    try {
+      const contactMailOptions = {
+        from: config.emailFrom,
+        to: config.contactEmailto,
+        cc: data.email,
+      };
+      const mailData = {
+        ...contactMailOptions,
+        subject: `Contact form: ${data.subject} by-${data.company} ${data.name}`,
+        html: contactFormTemplate(data),
+      };
+      await transporter.sendMail(mailData);
+
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      return res.status(400).json({ message: "Bad request" });
+    }
+  }
+
+  return res.status(400).json({ message: "Bad request" });
+};
 
 const careerForm = async (req, res, next) => {
-    //res.send('call send Email api')
-    try {
-        const { data } = req?.body
-        const files = req?.files ? req.files : null
-        if (!!data) {
-            const email = JSON.parse(data)
-            
-            if (!email.name || !email.email || !email.title || !email.mobile) {
-                return res.status(422).json({ message: 'content missed' });
-            }
+  //res.send('call send Email api')
+  try {
+    const { data } = req?.body;
+    const files = req?.files ? req.files : null;
+    if (!!data) {
+      const email = JSON.parse(data);
 
-            if (email.name === '400: test form fail') {
-                return res.status(400).json({ message: 'Bad request' })
-            }
+      if (!email.name || !email.email || !email.title || !email.mobile) {
+        return res.status(422).json({ message: "content missed" });
+      }
 
-            const mailOptions = {
-                from: config.emailserver,
-                to: config.emailto,
-                cc: email.email,
-                subject: email.subject || `Employment application form: ${email.name}`,
-                attachments: files,
-                html: careerFormTemplate(email),
-            }
-            await transporter.sendMail(mailOptions, (err, info) => {
-                if (err) {
-                    res.status(400).send(err)
-                } else {
-                    res.status(200).send('Email Send: ' + info.response)
-                }
-            })
+      if (email.name === "400: test form fail") {
+        return res.status(400).json({ message: "Bad request" });
+      }
+
+      const mailOptions = {
+        from: config.emailFrom,
+        to: config.emailto,
+        cc: email.email,
+        subject: email.subject || `Employment application form: ${email.name}`,
+        attachments: files,
+        html: careerFormTemplate(email),
+      };
+      await transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          res.status(400).send(err);
         } else {
-            res.status(400).send('no data: ' + JSON.stringify(req))
+          res.status(200).send("Email Send: " + info.response);
         }
-    } catch (error) {
-        res.status(400).send(error)
+      });
+    } else {
+      res.status(400).send("no data: " + JSON.stringify(req));
     }
-}
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
 module.exports = {
-    contactForm,
-    careerForm,
-    debugView
-}
+  contactForm,
+  careerForm,
+  debugView,
+};
--- helpers/config.js
+++ helpers/config.js
@@ -5,7 +5,7 @@ const assert = require('assert')
 
 dotenv.config()
 
-const { PORT, HOST, HOST_URL, EMAIL_SERVER, EMAIL_TO, PASSWORD, API_URL, CONTACT_EMAIL_TO, FRONTEND_DOMAIN } = process.env
+const { PORT, HOST, HOST_URL, EMAIL_SERVER, EMAIL_TO, PASSWORD, API_URL, CONTACT_EMAIL_TO, FRONTEND_DOMAIN, EMAIL_FROM } = process.env
 assert(PORT, 'PORT is required')
 assert(HOST, 'HOST is required')
 
@@ -14,6 +14,7 @@ module.exports = {
     host: HOST,
     url: HOST_URL,
     emailserver: EMAIL_SERVER,
+    emailFrom: EMAIL_FROM,
     emailto: EMAIL_TO,
     password: PASSWORD,
     apiUrl: API_URL,
